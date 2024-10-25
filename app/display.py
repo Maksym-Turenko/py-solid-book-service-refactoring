f0b87a0b6d34adea7b707a77c4e23d21eac540c4
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-25 18:21:23 +0300

    implement py-solid

---
+++ app/display.py
@@ -0,0 +1,8 @@
+class BookDisplay:
+    def display(self, book, display_type: str) -> None:
+        if display_type == "console":
+            print(book.content)
+        elif display_type == "reverse":
+            print(book.content[::-1])
+        else:
+            raise ValueError(f"Unknown display type: {display_type}")
